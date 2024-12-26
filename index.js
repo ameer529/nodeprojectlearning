d3e6ba904d36834baf2cafaf49024d34b26e0273
Ameer Hamza <ameer.hamza@techcreatix.com>
2024-12-26 18:48:11 +0500

    switch cases

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const http = require("http")
 const fs = require("fs")
 const myserver = http.createServer((req, res) => {
     const log = `${Date.now()}: Request Received with URL: ${req.url}\n`;
-    fs.appendFile("logs.txt", log, (err, data) => {
-        
-    })
+    fs.appendFile("logs.txt", log, (err, data) => {})
+    switch('${req.url}') {
+        case "/":
+            res.end("Homepage")
+            // Code block for value1
+            break;
+        case '/contact':
+            res.end("Conact Us Page")
+            // Code block for value2
+            break;
+        default:
+            res.end("404 not found")
+            // Code block if no case matches
+    }
     // console.log("Request Headers received.")
     // console.log(req.headers)
     // console.log("Request Body received.")
     // console.log(req.body)
     // console.log("Request received.")
     // console.log(req)
-    res.end("Hello from Backend")
 })
 
 myserver.listen(3000, ()=> console.log ("Server Started"))
