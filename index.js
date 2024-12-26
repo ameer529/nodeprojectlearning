cc514936953b3bef03a9e2c20af27b6dccdbefdd
Ameer Hamza <ameer.hamza@techcreatix.com>
2024-12-26 18:42:39 +0500

    logs and date function

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const http = require("http")
+const fs = require("fs")
 const myserver = http.createServer((req, res) => {
-    console.log("Request Headers received.")
-    console.log(req.headers)
-    console.log("Request Body received.")
-    console.log(req.body)
+    const log = `${Date.now()}: Request Received with URL: ${req.url}\n`;
+    fs.appendFile("logs.txt", log, (err, data) => {
+        
+    })
+    // console.log("Request Headers received.")
+    // console.log(req.headers)
+    // console.log("Request Body received.")
+    // console.log(req.body)
+    // console.log("Request received.")
+    // console.log(req)
     res.end("Hello from Backend")
 })
 
