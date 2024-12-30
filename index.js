a51c0bb456d58fca6b5fb4375ca721442c826df5
Ameer Hamza <ameer.hamza@techcreatix.com>
2024-12-30 16:03:23 +0500

    handling URLs

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 const myserver = http.createServer((req, res) => {
     const log = `${Date.now()}: Request Received with URL: ${req.url}\n`;
     fs.appendFile("logs.txt", log, (err, data) => {})
-    switch('${req.url}') {
+    const myurl = url.parse(req.url, true)
+    console.log(myurl.pathname)
+    switch(myurl.pathname) {
         case "/":
             res.end("Homepage")
             // Code block for value1
             break;
-        case '/contact':
-            res.end("Conact Us Page")
+        case '/about':
+            res.end(`Hi ${myurl.query.key1}, About Us Page`)
             // Code block for value2
             break;
         default:
